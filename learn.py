a71819947886b2142e7acd57aabaa812e7b0251f
oludav1920 <igbekeleholumide1@gmail.com>
2025-07-31 13:49:06 +0100

    work

---
+++ learn.py
@@ -0,0 +1,9 @@
+email = ("oludvav")
+country = ("usa")
+print("your email is "+ email + " and country " + country)
+
+if email =="" or country =="":
+    print("please fill email and country")
+
+elif email !="" and country !="":
+    print("succesful")
