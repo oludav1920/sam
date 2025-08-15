5bb0fc83eb87c2cc420e4317babf0ea87bac6bf1
oludav1920 <igbekeleholumide1@gmail.com>
2025-08-15 17:11:48 +0100

    visualisation work

---
+++ data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+#import matplotlib.use("Agg")
+xAxis = np.array([0,10])
+yAxis = np.array([0,300])
+plt.plot(xAxis, yAxis)
+plt.show()
