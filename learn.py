179be35fc619ff446e8c78970e9dba2fd37e30a5
oludav1920 <igbekeleholumide1@gmail.com>
2025-08-15 08:27:20 +0100

    mywork

--- learn.py
+++ learn.py
@@ -20,6 +20,14 @@ nameoffile = input("Enter your file name")
 writein = input("Enter content")
 x = open(nameoffile,"w")
 x.write(writein)
+print("success")
+query = input("yes or no")
+if query == "yes":
+    print("session continue")
+elif query == "no":
+    os.remove(nameoffile)
+else:
+    print("try again")
 x.close()
 
 '''x=2
